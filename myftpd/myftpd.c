294888bdc5d96ceb3ecc4dbd77167961c1290122
JunHao86 <106013710+JunHao86@users.noreply.github.com>
2022-07-28 00:06:14 +0800

    270722 - Added logger command

--- myftpd/myftpd.c
+++ myftpd/myftpd.c
@@ -16,10 +16,10 @@
 #include  <string.h>
 #include  <unistd.h> 
 #include  <sys/stat.h>
-
+#include  <fcntl.h>
 /*
 #include <dirent.h>
-#include <fcntl.h>
+
 #include <time.h>
 #include <stdarg.h>
 #include <signal.h>
@@ -29,7 +29,7 @@
 
 #define BUFSIZE         256
 #define FILE_BLOCK_SIZE 512  
-#define SERV_TCP_PORT 40004 //Listening TCP Port Number
+#define SERV_TCP_PORT 40228 // Assigned Listening TCP Port Number
 #define LOGPATH "/myftpd.log" //Logfile for daemon process
 
 //================================================================
@@ -40,6 +40,7 @@
  * 2. Function - daemon_init()
  * 3. File directory setting
  * 4. Standard server stuff - adapted from ser6.c
+ * 5. Function - logger()
  */
 
 /*
@@ -47,8 +48,6 @@
  * 1. Functions - serve_a_client()
  * 2. Write any other functions if required
  * 3. Uncheck any headers if necessary 
- * 4. Get supposed FILE_BLOCK_SIZE
- * 5. Define LOGGER and determine what would be written into the filelog
  */
 
 //================================================================
@@ -57,7 +56,7 @@ void claim_children()
 {
     pid_t pid=1;
      
-    while (pid>0) { /* claim as many zombies as we can */
+    while (pid>0) { 
         pid = waitpid(0, (int *)0, WNOHANG); 
     } 
 }
@@ -77,32 +76,50 @@ void daemon_init(void)
         exit(0);                   
     }
     
-    /* child continues */
-    setsid();                      /* become session leader */
-    chdir("/");                    /* change working directory */
-    umask(0);                      /* clear file mode creation mask */
+    setsid();                      
+    chdir("/");                    
+    umask(0);                      
 
-    /* catch SIGCHLD to remove zombies from system */
-    act.sa_handler = claim_children; /* use reliable signal */
-    sigemptyset(&act.sa_mask);       /* not to block other signals */
-    act.sa_flags   = SA_NOCLDSTOP;   /* not catch stopped children */
+    
+    act.sa_handler = claim_children; 
+    sigemptyset(&act.sa_mask);       
+    act.sa_flags   = SA_NOCLDSTOP;   
     sigaction(SIGCHLD,(struct sigaction *)&act,(struct sigaction *)0);
 }
 
+void logger(char* log, char* description)
+{
+    FILE *logfile;
+    logfile = fopen(log,"a+");
+    if(!logfile)
+    {
+        fprintf(stderr, "Unable to create or write to log file %s\n",log);
+        perror ("unable to create or write to log file\n");
+        exit(1);
+    }
+    fprintf(logfile,"%s",description);
+    fprintf(logfile,"\n");
+    fflush(logfile);
+}
+
 void serve_a_client(int sd)
-{   int nr, nw;
+{   int nr,nw; 
     char buf[FILE_BLOCK_SIZE];
 
-    while (1){
-         /* read data from client */
-         if ((nr = read(sd, buf, sizeof(buf))) <= 0) 
-             exit(0);   /* connection broken down */
-
-         /* ================= process data =================== */
-         //buf[nr] = '\0'; reverse(buf);
+    while (1)
+    {
+        /* read data from client */
+        if ((nr = read(sd, buf, sizeof(buf))) <= 0)
+        {
+            exit(0);   /* connection broken down */
+        }
+            
+        /* ================= process data =================== */
+        buf[nr] = '\0';
+        //reverse(buf);
               
-         /* send results to client */
-         nw = write(sd, buf, nr);
+        /* send results to client */
+        nw = write(sd, buf, nr);
     } 
 }
 
@@ -116,13 +133,16 @@ argc == 2   - User specifies initial current directory
 int main(int argc, char *argv[])
 {
     pid_t pid;
-    int sd,nsd,n; //what is the purpose of n?
+    int sd,nsd; //what is the purpose of n?
     socklen_t cli_addrlen;
     struct sockaddr_in ser_addr, cli_addr;
     unsigned short port = SERV_TCP_PORT;
 
     char initial_directory[BUFSIZE] = ".";
     char current_directory[BUFSIZE];
+    char logfile[256];
+
+    
     //get current directory
     getcwd(current_directory,sizeof(current_directory));
     
@@ -142,13 +162,26 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    /* setting path for logfile */
+    getcwd(current_directory,sizeof(current_directory));
+    strcpy(logfile,current_directory);
+    strcat(logfile,LOGPATH); //e.g. "./myftpd.log" , "./home/myftpd.log"
+
+    /*announces whenever program is started up, prevents confusion across clients*/
+    logger(logfile,"\n======Welcome to myftpd======\n"); 
+    
+
     /* turn the program into a daemon */
     daemon_init(); 
+
+    logger(logfile,"Server initialized");
+    
+    //logger(logfile,"initial directory set to %s", current_directory);
 
     /* set up listening socket sd */
     if ((sd = socket(PF_INET, SOCK_STREAM, 0)) < 0) 
     {
-        perror("server:socket");
+        perror("server: socket error");
         exit(1);
     } 
 
@@ -161,12 +194,13 @@ int main(int argc, char *argv[])
     /* bind server address to socket sd */
     if (bind(sd, (struct sockaddr *) &ser_addr, sizeof(ser_addr))<0)
     {
-        perror("server bind"); 
+        perror("server: bind failure"); 
         exit(1);
     }
 
     /* become a listening socket */
     listen(sd, 5);
+    
 
     while (1) {
 
@@ -175,11 +209,14 @@ int main(int argc, char *argv[])
         nsd = accept(sd, (struct sockaddr *) &cli_addr, (socklen_t *)&cli_addrlen);
         if (nsd < 0) 
         {
-            if (errno == EINTR)   /* if interrupted by SIGCHLD */
+            if (errno == EINTR) /* if interrupted by SIGCHLD */
+            {
                 continue;
-            perror("server:accept"); 
+            }   
+            perror("server: accept error"); 
             exit(1);
         }
+        logger(logfile,"Now listening to a client");
 
         /* create a child process to handle this client */
         if ((pid=fork()) <0) 
@@ -196,6 +233,7 @@ int main(int argc, char *argv[])
         /* now in child, serve the current client */
         close(sd); 
         serve_a_client(nsd);
+        logger(logfile,"Child has finished serving the client");
         exit(0);
     }
 }
