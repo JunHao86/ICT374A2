3308bf33545711d93d0f67a77b154bd1a5acf14f
JunHao86 <106013710+JunHao86@users.noreply.github.com>
2022-07-28 21:36:56 +0800

    Added client sided lpwd, ldir, lcd functions

--- myftp/myftp.c
+++ myftp/myftp.c
@@ -6,29 +6,125 @@
 
 #include  <netinet/in.h>       /* struct sockaddr_in, htons, htonl */
 #include  <netdb.h>            /* struct hostent, gethostbyname() */ 
-
 #include  <stdio.h>   
 #include  <stdlib.h>    
 #include  <string.h>
 #include  <sys/types.h>        
 #include  <sys/socket.h>
 #include  <unistd.h>    
-#include "token.h"            /* client commands cd,lcd,get,put */            
+#include  <dirent.h>           /* client commands dir, ldir */
+#include  "token.h"            /* client commands cd,lcd,get,put */            
 
 //================================================================
 
 #define SERV_TCP_PORT 40228 //Assigned Listening TCP Port Number
 #define BUFSIZE         256
 #define MAX_TOKENS        2 //Token 1 : Command name
-                            //Token 2 : Filename/Pathname (if using cd,lcd,get,put)
-
+                            //Token 2 : Filename/Pathname
 //================================================================
 
 /*
  * To do list
- * 1. Write functions for client commands
+ * 1. Write functions for client-server commands
  * 2. Uncheck dirent.h    sys/stat.h    fcntl.h if necessary 
  */
+
+//================================================================
+
+/*
+ * Client Side Commands
+ * 1. func_lpwd - to display current working directory of the client
+ * 2. func_ldir - to display the file names under the current directory
+ *                of the client
+ * 3. func_lcd  - to change the current directory of the client
+ */
+
+void func_lpwd()
+{
+    char working_directory[BUFSIZE];
+
+    getcwd(working_directory,sizeof(working_directory));
+    printf("%s\n",working_directory);
+}
+ 
+void func_ldir(char* token1)
+{
+	DIR *dir;
+	struct dirent *directory;
+
+    if(token1 == NULL) //if no tokens specified, set as cwd
+    {
+		token1 = ".";
+	}
+	if ((dir = opendir(token1))!= NULL)
+    {
+    	while ((directory = readdir(dir)) != NULL)
+        {
+	        printf("Filename: %s\n", directory->d_name);
+	    }
+	    closedir(dir);
+	}
+    else
+    {
+        perror("dirent error");
+    }
+}
+
+void func_lcd(char* token1)
+{
+    char working_directory[BUFSIZE];
+    if(chdir(token1)==0)
+    {
+        getcwd(working_directory,sizeof(working_directory));
+        printf("Directory changed to : %s\n",working_directory);
+    }
+    else
+    {
+        perror("unable to change to specified directory");
+    }
+}
+
+//================================================================
+
+/*
+ * Server Side Commands
+ * 1. func_pwd - to display the current directory of the server
+ *               that is serving the client
+ * 2. func_dir - to display the file names under the current directory 
+ *               of the server that is serving the client
+ * 3. func_cd  - to change the current directory 
+ *               of the server that is serving the client
+ * 4. func_get - to download the named file from the current directory 
+ *               of the remote server 
+ *               and save it in the current directory of the client
+ * 5. func_put - to upload the named file from the current directory of 
+ *               the client to the current directory of the remove server
+ */
+
+void func_pwd(int sd, char* token)
+{
+
+}
+
+void func_dir(int sd, char* token)
+{
+
+}
+
+void func_cd(int sd, char* token)
+{
+
+}
+
+void func_get(int sd, char* filename)
+{
+
+}
+
+void func_put(int sd, char* filename)
+{
+
+}
 
 //================================================================
 
@@ -60,25 +156,25 @@ int main(int argc, char *argv[])
         exit(1); 
     }
 
-    // getting host address, building server socket address
+    //getting host address, building server socket address
     bzero((char *) &ser_addr, sizeof(ser_addr));
     ser_addr.sin_family = AF_INET;
     ser_addr.sin_port = htons(SERV_TCP_PORT);
-    if ((hp = gethostbyname(host)) == NULL){
-        printf("host %s not found\n", host); exit(1);   
+    if ((hp = gethostbyname(host)) == NULL)
+    {
+        printf("host %s not found\n", host); 
+        exit(1);   
     }
     ser_addr.sin_addr.s_addr = * (u_long *) hp->h_addr;
 
-    /* create TCP socket & connect socket to server address */
+    //create TCP socket & connect socket to server address
     sd = socket(PF_INET, SOCK_STREAM, 0);
-    if (connect(sd, (struct sockaddr *) &ser_addr, sizeof(ser_addr))<0) { 
-        perror("client connect"); exit(1);
+    if (connect(sd, (struct sockaddr *) &ser_addr, sizeof(ser_addr))<0) 
+    { 
+        perror("client connect"); 
+        exit(1);
     }
     
-    /*
-    client program from here
-    */
-
     while(1) 
     {
         printf(">   "); //display prompt
@@ -99,42 +195,39 @@ int main(int argc, char *argv[])
             printf("Terminating myftp session\n");
             exit(0);
         }
-        else if(strcmp(tokenArray[0],"pwd")==0)
+        else if(strcmp(tokenArray[0],"pwd")==0) //server side command 1
         {
             printf("pwd session\n");
             //function here
         }
-        else if(strcmp(tokenArray[0],"lpwd")==0)
+        else if(strcmp(tokenArray[0],"lpwd")==0) 
         {
-            printf("lpwd session\n");
-            //function here
+            func_lpwd();
         }
-        else if(strcmp(tokenArray[0],"dir")==0)
+        else if(strcmp(tokenArray[0],"dir")==0) //server side command 2
         {
             printf("dir session\n");
             //function here
         }
-        else if(strcmp(tokenArray[0],"ldir")==0)
+        else if(strcmp(tokenArray[0],"ldir")==0) 
         {
-            printf("ldir session\n");
-            //function here
+            func_ldir(tokenArray[1]);
         }
-        else if(strcmp(tokenArray[0],"cd")==0)
+        else if(strcmp(tokenArray[0],"cd")==0) //server side command 3
         {
             printf("cd session\n");
             //function here
         }
-        else if(strcmp(tokenArray[0],"lcd")==0)
+        else if(strcmp(tokenArray[0],"lcd")==0) 
         {
-            printf("lcd session\n");
-            //function here
+            func_lcd(tokenArray[1]);
         }
-        else if(strcmp(tokenArray[0],"get")==0)
+        else if(strcmp(tokenArray[0],"get")==0) //server side command 4
         {
             printf("get session\n");
             //function here
         }
-        else if(strcmp(tokenArray[0],"put")==0)
+        else if(strcmp(tokenArray[0],"put")==0) //server side command 5
         {
             printf("put session\n");
             //function here
@@ -142,8 +235,10 @@ int main(int argc, char *argv[])
         else
         {
             printf("Invalid command, here are the available commands: \n");
-            printf("Standalone: pwd, lpwd, dir, ldir, quit\n");
-            printf("Require filename/pathname: cd, lcd, get, put\n");
+            printf("Requires a filename/pathname: cd, lcd, get, put\n");
+            printf("\n");
+            printf("Client sided commands: lpwd, ldir, lcd\n");
+            printf("Server sided commands: pwd, dir, cd, get, put\n");
         }
     }
     exit(0);
